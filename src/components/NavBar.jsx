bb9556cfcb64b1e55254f563b4149bc7264333b5
Rodymus <robertogonzalez.ar@gmail.com>
2023-05-19 16:18:35 -0300

    se agrego un menu desplegable en las opciones de vino

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+
 import CartWidget from './CartWidget'
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import NativeSelect from '@mui/material/NativeSelect';
 
 const NavBar = () => {
   return (
     <div>
     <h1>Tienda de Vinos</h1>
-    <ul>
-        <button>Tinto</button>
-        <button>Blanco</button>
-        <button>Rosado</button>
-    </ul>
+
+    <Box sx={{ minWidth: 60, maxWidth: 130 }}>
+      <FormControl fullWidth>
+        <InputLabel>
+          Seleccione el vino
+        </InputLabel>
+        <NativeSelect autoWidth >
+          <option>Tinto</option>
+          <option>Blanco</option>
+          <option>Rosado</option>
+        </NativeSelect>
+      </FormControl>
+    </Box>
+
     <CartWidget />
     </div>
   )
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,6 +1,4 @@
-import { FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField } from "@mui/material"
-import Button from '@mui/material/Button'
-import Box from '@mui/material/Box'
+import { Box, Button, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField } from "@mui/material"
 import { Visibility, VisibilityOff } from "@mui/icons-material"
 import { useState } from "react"
 
@@ -12,22 +10,32 @@ const Login = () => {
     <Box sx={ { width: "100%", alignItems: "center", display: "flex", minHeight: "100vh", justifyContent: "center" }}>
         <TextField  label="Email" variant="outlined" color="secondary" />
         
-        <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            type={showPassword ? 'text' : 'password'}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={()=> setShowPassword(!setShowPassword)}
-                  edge="end">
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>}
-            label="Password"/>
-        </FormControl>
+        <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
+        <InputLabel color="secondary" htmlFor="outlined-adornment-password">
+          Contraseña
+        </InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-password"
+          color="secondary"
+          type={showPassword ? "text" : "password"}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={() => setShowPassword(!showPassword)}
+                edge="end"
+              >
+                {showPassword ? (
+                  <VisibilityOff />
+                ) : (
+                  <Visibility color="secondary" />
+                )}
+              </IconButton>
+            </InputAdornment>
+          }
+          label="Password"
+        />
+      </FormControl>
 
         <Button variant="contained" size="medium" color="secondary" >Ingresar</Button>
     </Box>
